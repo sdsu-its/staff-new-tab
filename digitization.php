d551919271ca4e50e8bb6b411f7d00efb0024901
Tom Paulus <tom@tompaulus.com>
2016-12-09 14:10:33 -0800

    Removed Broken Link and Centralized Stylesheet.

---
+++ digitization.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+    <title>Digitization Intake Tools</title>
+    <link href="style.css" type="text/css">
+</head>
+<body>
+<h1>Digitization Tools</h1>
+<table align="center">
+    <tr>
+        <td class="header">
+            <h2>Workflow</h2>
+        </td>
+        <td class="header">
+            <h2>Resources</h2>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <ul class="tools">
+                <li>
+                    <a href="digitization-protocol.pdf" class="red">
+                        <span class="title">Digitization Protocol</span>
+                        <span class="description">&nbsp;</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://docs.google.com/forms/d/1m-j3Ep59cf5h3ISETvP3Aqrw9id0x5UO1OGJUzSXWec/viewform"
+                       class="gold">
+                        <span class="title">Intake Form</span>
+                        <span class="description">Submit Media Information to Video Team</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://sdsu-its.desk.com/agent" class="black">
+                        <span class="title">Desk.com</span>
+                        <span class="description">Support Tickets</span>
+                    </a>
+                </li>
+            </ul>
+        </td>
+        <td>
+            <ul class="tools">
+                <li>
+                    <a href="intake-form.pdf" class="red">
+                        <span class="title">Printable Request Form</span>
+                        <span class="description">&nbsp;</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://mediasite.sdsu.edu/Mediasite/Login" class="red">
+                        <span class="title">MediaSite</span>
+                        <span class="description">Search for Media in our New System</span>
+                    </a>
+                </li>
+            </ul>
+        </td>
+    </tr>
+</table>
+
+
+<footer>
+    <p id="copyright">&copy; <?php echo date("Y"); ?> SDSU - Instructional Technology Services
+    <p id="disclaimer">The statements found on this page/site are for informational purposes only. While every effort is
+        made to ensure that this information is up to date and accurate, official information can be found in the
+        university publications.</p>
+</footer>
+</body>
+</html>
